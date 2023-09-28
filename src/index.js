4afba8322138b20036894130f4ee0f48fb6a8d81
Viktor Belov <vbelov@vansofts.net>
2023-09-28 11:58:26 +0300

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const openSet = new Set();
+  const closeSet = new Set();
+  const sameSet = new Set();
+
+  for (const [open, close] of bracketsConfig) {
+    if (open === close) {
+      sameSet.add(open);
+    } else {
+      openSet.add(open);
+      closeSet.add(close);
+    }
+  }
+
+  const stack = [];
+
+  for (const bracket of str) {
+    if (sameSet.has(bracket)) {
+      if (stack.length > 0 && stack[stack.length - 1] === bracket) {
+        stack.pop();
+      } else {
+        stack.push(bracket);
+      }
+    } else if (openSet.has(bracket)) {
+      stack.push(bracket);
+    } else if (closeSet.has(bracket)) {
+      const last = stack.pop();
+      if (!last || bracketsConfig.find(([open, close]) => open === last && close === bracket) === undefined) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
